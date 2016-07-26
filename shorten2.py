046bd1800d14a8019f0f69320e19a9945f364b55
ryanhgunn <ryanhgunn@gmail.com>
2016-07-26 15:10:55 -0700

    Some Python stuff.

---
+++ shorten2.py
@@ -0,0 +1,24 @@
+import sys
+
+orig = sys.argv[1]
+short = ""
+count = 1
+letter = ""
+
+for i in range(0, int(len(orig))):
+    if letter != orig[i]:
+        letter = orig[i]
+        if count != 1:
+            short += str(count)
+        short += letter
+        count = 1
+    else:
+        count += 1
+
+if count != 1:
+    short += str(count)
+
+if int(len(short)) < int(len(orig)):
+    print(short)
+else:
+    print(orig)
